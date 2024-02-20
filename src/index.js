af6ae687429a5931469c5b9f12635daab188a0ce
taniagabis98 <t.gabis@experienceco.com>
2024-02-20 16:51:09 +1100

    Stuck on temperature change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector(".search-form-input");
-  let cityElement = document.querySelector(".weather-app-city");
-  cityElement.textContent = searchInput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.main.temp;
+  let cityElement = document.querySelector("#city");
+  cityElement.textContent = response.data.city;
+
+  temperatureElement.textContent = Math.round(temperature);
 }
 
-let searchFormElement = document.querySelector(".search-form");
+function searchCity(city) {
+  let apiKey = "0fabbb54t5ce773e761073oa14a077f0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
