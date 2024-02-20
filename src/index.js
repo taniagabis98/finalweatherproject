ccbe01677328ff3a7d95acbafe9e73092ab03f97
taniagabis98 <t.gabis@experienceco.com>
2024-02-20 17:25:16 +1100

    JS added the rest of the info including date and weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,37 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.textContent = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  cityElement.textContent = response.data.city;
+  timeElement.textContent = formatDate(date);
+  descriptionElement.textContent = response.data.condition.description;
+  humidityElement.textContent = `${response.data.temperature.humidity}%`;
+  windSpeedElement.textContent = `${response.data.wind.speed} km/h`;
   temperatureElement.textContent = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes};`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 }
 
 function searchCity(city) {
