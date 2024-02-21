b20b244ca1d19954f748ba7581cc588040d4d067
taniagabis98 <t.gabis@experienceco.com>
2024-02-21 11:43:35 +1100

    Updated JS with innerHTML

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ function refreshWeather(response) {
 
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 
-  cityElement.textContent = response.data.city;
-  timeElement.textContent = formatDate(date);
-  descriptionElement.textContent = response.data.condition.description;
-  humidityElement.textContent = `${response.data.temperature.humidity}%`;
-  windSpeedElement.textContent = `${response.data.wind.speed} km/h`;
-  temperatureElement.textContent = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function formatDate(date) {
