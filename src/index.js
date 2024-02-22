83c55de577ff1d569a649c386b802730dd6d41f6
taniagabis98 <t.gabis@experienceco.com>
2024-02-22 11:08:34 +1100

    we good js

--- src/index.js
+++ src/index.js
@@ -56,3 +56,19 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Melbourne");
+
+let forecast = document.querySelector("#forcast");
+forecast.innerHTML = `
+  <div class="weather-forcast-day">
+    <div class="weather-forecast-date">Fri</div>
+    <img
+      src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
+      alt=""
+      width="50"
+    />
+    <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temperature-max">38°</span>
+      <span class="weather-forecast-temperature-min">12°</span>
+    </div>
+  </div>
+`;
